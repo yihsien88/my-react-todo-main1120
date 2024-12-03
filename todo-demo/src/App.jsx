3afe472e85c4670f664f05781b428520fe10954a
yihsien88 <k1564ann@gmail.com>
2024-12-03 09:37:24 +0800

    1203

--- todo-demo/src/App.jsx
+++ todo-demo/src/App.jsx
@@ -1,12 +1,87 @@
-import "./App.css"
-import TodoWrapper from "./components/TodoWrapper";
+import { useEffect } from "react"
+import axios from 'axios';
+import './App.css'
 
 function App() {
-  return (
-    <>
-      <TodoWrapper />
-    </>
-  )
+    // useEffect(() => {
+    //     (async () => {
+    //         const data = await axios.get('./F-C0032-001.json');
+    //         const { location } = data.data.cwaopendata.dataset;
+    //         console.log(location);
+    //     })
+    // }, [])
+
+    return (
+        <>
+            <div className="wrap">
+                <h2>36小時天氣預報</h2>
+                {/* 一列兩欄 */}
+                <div className="row">
+                    <div className="col">
+                        {/* 卡片樣式 */}
+                        <div className="card">
+                            {/* 標題 */}
+                            <div className="card-title">臺北市</div>
+                            {/* 內容 */}
+                            <div className="card-body">
+                                {/* 一式三欄 */}
+                                <div className="row2">
+                                    {/* 第一欄 */}
+                                    <div className="col2">
+                                        <p> 2日</p>
+                                        <p>
+                                            上午6:00 <br />
+                                            ~<br />
+                                            下午6:00
+                                        </p>
+                                        <p>
+                                            <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                                        </p>
+                                        <p>晴時多雲</p>
+                                        <p>10%</p>
+                                    </div>
+
+                                    {/* 第二欄 */}
+                                    <div className="col2">
+                                        <p> 2日</p>
+                                        <p>
+                                            上午6:00 <br />
+                                            ~<br />
+                                            下午6:00
+                                        </p>
+                                        <p>
+                                            <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                                        </p>
+                                        <p>晴時多雲</p>
+                                        <p>10%</p>
+                                    </div>
+
+                                    {/* 第三欄 */}
+                                    <div className="col2">
+                                        <p> 2日</p>
+                                        <p>
+                                            上午6:00 <br />
+                                            ~<br />
+                                            下午6:00
+                                        </p>
+                                        <p>
+                                            <img src="./public/weatherIcon/晴時多雲.svg" alt="" />
+                                        </p>
+                                        <p>晴時多雲</p>
+                                        <p>10%</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col">第二欄</div>
+                </div>
+
+
+            </div>
+
+        </>
+    )
 }
 
 
