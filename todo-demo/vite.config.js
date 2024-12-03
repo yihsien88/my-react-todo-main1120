66fc0721fdc708c2371e98f597d6c2da4c561baa
yihsien88 <k1564ann@gmail.com>
2024-12-03 10:11:10 +0800

    1203

--- todo-demo/vite.config.js
+++ todo-demo/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
   base: process.env.NODE_EW === 'production'?'/my-react-todo-main1120/' :'/',
+  plugins: [react()],
 })
